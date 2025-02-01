0db66017b2e9d2bc1882841508444709438c9f45
striderjg <JohnG27@yahoo.com>
2025-01-31 22:21:45 -0800

    Ch1-5LAll

---
+++ main.go
@@ -0,0 +1,305 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync/atomic"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/striderjg/chirpy/internal/database"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
+	platform       string
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(rw, req)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type:", "text/html")
+	page := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
+	w.Write([]byte(page))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		errorResponce(403, "Not Authorized", w)
+		return
+	}
+
+	cfg.fileserverHits.Store(0)
+	if err := cfg.dbQueries.DeleteUsers(r.Context()); err != nil {
+		log.Printf("Error deleting users: %s", err)
+		errorResponce(500, "Error DeleteUsers", w)
+		return
+	}
+	w.Write([]byte{})
+}
+
+func (cfg *apiConfig) handleUsers(w http.ResponseWriter, r *http.Request) {
+	type paramaters struct {
+		Email string `json:"email"`
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	var params paramaters
+	if err := decoder.Decode(&params); err != nil {
+		log.Printf("Error decoding paramaters: %s", err)
+		errorResponce(500, "Something went wrong", w)
+		return
+	}
+
+	if len(params.Email) == 0 {
+		errorResponce(400, "Must send an email address", w)
+	}
+	usr, err := cfg.dbQueries.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		errorResponce(400, "Unable to create user", w)
+	}
+	retUser := User{
+		ID:        usr.ID,
+		CreatedAt: usr.CreatedAt,
+		UpdatedAt: usr.UpdatedAt,
+		Email:     usr.Email,
+	}
+	jsonResponce(201, retUser, w)
+}
+
+func (cfg *apiConfig) handleGetChirps(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	chirps, err := cfg.dbQueries.GetChirps(r.Context())
+	if err != nil {
+		log.Printf("Error Fetching Chirps\n")
+		errorResponce(500, "Something went wrong", w)
+		return
+	}
+	retChirps := make([]Chirp, len(chirps))
+	for i, chirp := range chirps {
+		retChirps[i].ID = chirp.ID
+		retChirps[i].CreatedAt = chirp.CreatedAt
+		retChirps[i].UpdatedAt = chirp.UpdatedAt
+		retChirps[i].Body = chirp.Body
+		retChirps[i].UserID = chirp.UserID
+	}
+	jsonResponce(200, retChirps, w)
+}
+
+func (cfg *apiConfig) handleGetChirpByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	//fmt.Println(r.PathValue())
+	id, err := uuid.Parse(r.PathValue("chirpID"))
+	if err != nil {
+		errorResponce(400, "Bad ID", w)
+		return
+	}
+	chirp, err := cfg.dbQueries.GetChripById(r.Context(), id)
+	if err != nil {
+		errorResponce(400, "Bad ID", w)
+		return
+	}
+	jsonResponce(200,
+		Chirp{
+			ID:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
+		},
+		w,
+	)
+}
+
+func (cfg *apiConfig) handleChirps(w http.ResponseWriter, r *http.Request) {
+	type paramaters struct {
+		Body   string    `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	var params paramaters
+	if err := decoder.Decode(&params); err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		errorResponce(500, "Something went wrong", w)
+		return
+	}
+
+	if len(params.Body) == 0 {
+		errorResponce(400, "No Chirp Recieved", w)
+		return
+	}
+	if len(params.UserID) == 0 {
+		errorResponce(400, "No User", w)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		errorResponce(400, "Chirp is too long", w)
+		return
+	}
+
+	// ============== Below eats extra white space.  Think it's ok for the tests but modifies output more then asked.
+
+	words := strings.Split(params.Body, " ")
+	filteredWords := make([]string, 0, len(words))
+	for _, word := range words {
+		lowerCaseWord := strings.ToLower(word)
+		if lowerCaseWord == "kerfuffle" || lowerCaseWord == "sharbert" || lowerCaseWord == "fornax" {
+			filteredWords = append(filteredWords, "****")
+		} else {
+			filteredWords = append(filteredWords, word)
+		}
+	}
+
+	chirp, err := cfg.dbQueries.CreateChirp(r.Context(), database.CreateChirpParams{
+		UserID: params.UserID,
+		Body:   strings.Join(filteredWords, " "),
+	})
+	if err != nil {
+		log.Printf("Error creating chirp: %s\n", err)
+		errorResponce(500, "Something went wrong", w)
+		return
+	}
+
+	ret := Chirp{
+		ID:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserID:    chirp.UserID,
+	}
+	jsonResponce(201, ret, w)
+}
+
+func handleValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type paramaters struct {
+		Body string `json:"body"`
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	decoder := json.NewDecoder(r.Body)
+	var params paramaters
+	if err := decoder.Decode(&params); err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		errorResponce(500, "Something went wrong", w)
+		return
+	}
+
+	if len(params.Body) == 0 {
+		errorResponce(400, "No Chirp Recieved", w)
+		return
+	}
+	if len(params.Body) > 140 {
+		errorResponce(400, "Chirp is too long", w)
+		return
+	}
+
+	// ============== Below eats extra white space.  Think it's ok for the tests but modifies output more then asked.
+
+	words := strings.Split(params.Body, " ")
+	filteredWords := make([]string, 0, len(words))
+	for _, word := range words {
+		lowerCaseWord := strings.ToLower(word)
+		if lowerCaseWord == "kerfuffle" || lowerCaseWord == "sharbert" || lowerCaseWord == "fornax" {
+			filteredWords = append(filteredWords, "****")
+		} else {
+			filteredWords = append(filteredWords, word)
+		}
+	}
+
+	type retType struct {
+		Cleaned_body string `json:"cleaned_body"`
+	}
+	ret := retType{
+		Cleaned_body: strings.Join(filteredWords, " "),
+	}
+	jsonResponce(200, ret, w)
+}
+
+func jsonResponce(code int, payload interface{}, w http.ResponseWriter) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshaling data")
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func errorResponce(code int, errorResponce string, w http.ResponseWriter) {
+	type errReturn struct {
+		Error string `json:"error"`
+	}
+	ret := errReturn{
+		Error: errorResponce,
+	}
+	data, err := json.Marshal(ret)
+	if err != nil {
+		log.Printf("Error Marshaling data")
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func main() {
+	apiCfg := apiConfig{}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	apiCfg.platform = os.Getenv("PLATFORM")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("error opening db: %w\n", err)
+		os.Exit(1)
+	}
+	apiCfg.dbQueries = database.New(db)
+
+	serverMux := http.NewServeMux()
+	serverMux.Handle(
+		"/app/",
+		http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))),
+	)
+
+	serverMux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
+	serverMux.HandleFunc("POST /api/users", apiCfg.handleUsers)
+	serverMux.HandleFunc("POST /api/chirps", apiCfg.handleChirps)
+	serverMux.HandleFunc("GET /api/chirps", apiCfg.handleGetChirps)
+	serverMux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handleGetChirpByID)
+
+	serverMux.HandleFunc("GET /admin/metrics", apiCfg.handleMetrics)
+	serverMux.HandleFunc("POST /admin/reset", apiCfg.handleReset)
+
+	serverMux.HandleFunc(
+		"GET /api/healthz",
+		func(rw http.ResponseWriter, req *http.Request) {
+			header := rw.Header()
+			header.Add("Content-Type", "text/plain; charset=utf-8")
+			rw.WriteHeader(200)
+			rw.Write([]byte("OK"))
+		},
+	)
+
+	server := http.Server{
+		Handler: serverMux,
+		Addr:    ":8080",
+	}
+
+	server.ListenAndServe()
+}
---
+++ sql/queries/chirps.sql
@@ -0,0 +1,16 @@
+-- name: CreateChirp :one
+INSERT INTO chirps (id, created_at, updated_at, body, user_id)
+VALUES (
+    gen_random_uuid(),
+    NOW(),
+    NOW(),
+    $2,
+    $1
+)
+RETURNING *;
+
+-- name: GetChirps :many
+SELECT * from chirps ORDER BY created_at;
+
+-- name: GetChripById :one
+select * from chirps WHERE id = $1;
